7dac4c762bc2aa60d09557f825aaf009fc59f760
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2022-01-14 12:02:04 +0000

    sdf

---
+++ Semester 1/Assets/ExternalAssets/MobilityArts/SciFiHandGun/Scripts/OrbitAroundTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAroundTarget : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start () {
+
+		//transform.SetParent(target.transform);
+	}
+
+	void Update () {
+
+		//transform.position = target.transform.position;
+
+	}
+}
